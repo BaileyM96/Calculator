c4441416da71cd938f9d0dff0979e4a5829c081d
Bailey <baileymejia28@gmail.com>
2024-02-04 18:37:15 -0800

    Created functions for basic functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+return a + b;
+}
+
+function subtract(a, b) {
+return a - b;
+}
+
+function multiply(a, b) {
+return a * b;
+}
+
+function divide(a, b) {
+return a / b;
+}
+
+//CREATE 3 Variables (3 + 5)
+    //SET Number
+    //SET Operator number
+    //SET Another number
+
+//CREATE fucntion operate();
+    //TAKES an operator and 2 numbers
+        //THEN Call the above fucntions
+
+function operate(num1, num2, operator) {
+    
+}
